a1ff7555ff6de09c8bae1c991007c2269c1390cb
lars1974 <lars.jensen.74@gmail.com>
2024-02-17 19:06:57 +0100

    piplien

--- dist/index.js
+++ dist/index.js
@@ -60057,6 +60057,7 @@ const core = __nccwpck_require__(2186)
 const { wait } = __nccwpck_require__(1312)
 const tc = __nccwpck_require__(7784)
 const cache = __nccwpck_require__(7799)
+const exec = __nccwpck_require__(1514)
 
 /**
  * The main function for the action.
@@ -60070,6 +60071,12 @@ async function run() {
       methodCalls.push(downloadTool(param))
     }
     await Promise.all(methodCalls)
+
+    await exec.exec('helm', [
+      'plugin',
+      'install',
+      'https://github.com/databus23/helm-diff'
+    ])
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
@@ -60109,64 +60116,12 @@ async function downloadTool(tool) {
     await tc.extractTar(await tc.downloadTool(tool.url), path)
     await cache.saveCache([path], tool.url)
   }
-  core.info('hmm1')
   const cachedPath = await tc.cacheDir(path, tool.name, tool.version)
-  core.info('hmm2')
   core.addPath(`${cachedPath}/${tool.pathToExecutable}`)
-  core.info('hmm3')
   if (tool.environmentVariable) {
-    core.info(`hmm4 tool.environmentVariable: ${tool.environmentVariable}`)
     const env = tool.environmentVariable.replace('CACHED_PATH', cachedPath)
-
     core.exportVariable(env.split('=')[0], env.split('=')[1])
-    core.info(
-      `hmm5${tool.environmentVariable.split('=')[0]}${tool.environmentVariable.split('=')[1]}`
-    )
   }
-  core.info('hmm6')
-  //await tool.exec(tool.verify)
-}
-
-async function downloadHelm() {
-  await cache.restoreCache(['tools/helm/3.14.1'], 'helm-3.14.1-linux-amd64', [])
-  if (
-    (await cache.restoreCache(
-      ['tools/helm/3.14.1'],
-      'helm-3.14.1-linux-amd64',
-      []
-    )) === undefined
-  ) {
-    core.info('Cache not found, downloading helm')
-    const helmURL = 'https://get.helm.sh/helm-v3.14.1-linux-amd64.tar.gz'
-    const helmPath = await tc.downloadTool(helmURL)
-    await tc.extractTar(helmPath, 'tools/helm/3.14.1')
-    const cachedPath = await tc.cacheDir('tools/helm/3.14.1', 'helm', '3.14.1')
-    await cache.saveCache(['tools/helm/3.14.1'], 'helm-3.14.1-linux-amd64')
-    core.addPath(`${cachedPath}/linux-amd64`)
-  } else {
-    core.info('Cache found, using cached helm')
-  }
-}
-
-async function downloadJava() {
-  const url =
-    'https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz'
-  const path = await tc.downloadTool(url)
-  const extractedFolder = await tc.extractTar(path, 'tools/java/21.0.2')
-  const cachedPath = await tc.cacheDir(extractedFolder, 'java', '21.0.2')
-
-  core.info(`cache path: ${cachedPath}`)
-  core.addPath(`${cachedPath}/jdk-21.0.2/bin`)
-  core.exportVariable('JAVA_HOME', `${cachedPath}/jdk-21.0.2/`)
-}
-
-async function downloadMaven() {
-  const url =
-    'https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz'
-  const path = await tc.downloadTool(url)
-  const extractedFolder = await tc.extractTar(path, 'tools/maven/3.9.6')
-  const cachedPath = await tc.cacheDir(extractedFolder, 'maven', '3.9.6')
-  core.addPath(`${cachedPath}/apache-maven-3.9.6/bin`)
 }
 
 module.exports = {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const core = require('@actions/core')
 const { wait } = require('./wait')
 const tc = require('@actions/tool-cache')
 const cache = require('@actions/cache')
+const exec = require('@actions/exec')
 
 /**
  * The main function for the action.
@@ -15,6 +16,12 @@ async function run() {
       methodCalls.push(downloadTool(param))
     }
     await Promise.all(methodCalls)
+
+    await exec.exec('helm', [
+      'plugin',
+      'install',
+      'https://github.com/databus23/helm-diff'
+    ])
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
@@ -54,64 +61,12 @@ async function downloadTool(tool) {
     await tc.extractTar(await tc.downloadTool(tool.url), path)
     await cache.saveCache([path], tool.url)
   }
-  core.info('hmm1')
   const cachedPath = await tc.cacheDir(path, tool.name, tool.version)
-  core.info('hmm2')
   core.addPath(`${cachedPath}/${tool.pathToExecutable}`)
-  core.info('hmm3')
   if (tool.environmentVariable) {
-    core.info(`hmm4 tool.environmentVariable: ${tool.environmentVariable}`)
     const env = tool.environmentVariable.replace('CACHED_PATH', cachedPath)
-
     core.exportVariable(env.split('=')[0], env.split('=')[1])
-    core.info(
-      `hmm5${tool.environmentVariable.split('=')[0]}${tool.environmentVariable.split('=')[1]}`
-    )
   }
-  core.info('hmm6')
-  //await tool.exec(tool.verify)
-}
-
-async function downloadHelm() {
-  await cache.restoreCache(['tools/helm/3.14.1'], 'helm-3.14.1-linux-amd64', [])
-  if (
-    (await cache.restoreCache(
-      ['tools/helm/3.14.1'],
-      'helm-3.14.1-linux-amd64',
-      []
-    )) === undefined
-  ) {
-    core.info('Cache not found, downloading helm')
-    const helmURL = 'https://get.helm.sh/helm-v3.14.1-linux-amd64.tar.gz'
-    const helmPath = await tc.downloadTool(helmURL)
-    await tc.extractTar(helmPath, 'tools/helm/3.14.1')
-    const cachedPath = await tc.cacheDir('tools/helm/3.14.1', 'helm', '3.14.1')
-    await cache.saveCache(['tools/helm/3.14.1'], 'helm-3.14.1-linux-amd64')
-    core.addPath(`${cachedPath}/linux-amd64`)
-  } else {
-    core.info('Cache found, using cached helm')
-  }
-}
-
-async function downloadJava() {
-  const url =
-    'https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz'
-  const path = await tc.downloadTool(url)
-  const extractedFolder = await tc.extractTar(path, 'tools/java/21.0.2')
-  const cachedPath = await tc.cacheDir(extractedFolder, 'java', '21.0.2')
-
-  core.info(`cache path: ${cachedPath}`)
-  core.addPath(`${cachedPath}/jdk-21.0.2/bin`)
-  core.exportVariable('JAVA_HOME', `${cachedPath}/jdk-21.0.2/`)
-}
-
-async function downloadMaven() {
-  const url =
-    'https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz'
-  const path = await tc.downloadTool(url)
-  const extractedFolder = await tc.extractTar(path, 'tools/maven/3.9.6')
-  const cachedPath = await tc.cacheDir(extractedFolder, 'maven', '3.9.6')
-  core.addPath(`${cachedPath}/apache-maven-3.9.6/bin`)
 }
 
 module.exports = {
